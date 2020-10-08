e4e09f42f28c9e9df282cc50ae245952c30ecd41
Marvie Salutan <maria.salutan@lanexus.com>
2020-10-08 21:09:33 +0800

    build:add latest build files

---
+++ build/precache-manifest.a976c816e1c26daf7176ac11593f8ebe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c0b1dc5c48a8de91e3b783b33601f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11bc53095838aaedeac8",
+    "url": "/static/css/main.172c408f.chunk.css"
+  },
+  {
+    "revision": "6bf3ab3c83e297b9cf40",
+    "url": "/static/js/2.691a5a87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.691a5a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11bc53095838aaedeac8",
+    "url": "/static/js/main.0aa1af4a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "f5e8f5de82a902531132b3307595a459",
+    "url": "/static/media/repeatable-background.f5e8f5de.png"
+  },
+  {
+    "revision": "39635620bc39fd89280af66645b69d80",
+    "url": "/static/media/run.39635620.gif"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4a89a7ae9b941166d6d519c94ebf9db.js"
+  "/precache-manifest.a976c816e1c26daf7176ac11593f8ebe.js"
 );
 
 self.addEventListener('message', (event) => {
